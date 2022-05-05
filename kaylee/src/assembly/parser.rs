d55241b90aaeadd8fc253c771e2e131c05189a95
Michael Wilson <mwilson@electricjones.me>
2022-05-05 10:53:56 -0400

    THe most basic parser

--- kaylee/src/assembly/parser.rs
+++ kaylee/src/assembly/parser.rs
@@ -8,37 +8,89 @@ use nom::error::{ErrorKind, VerboseError};
 use nom::multi::{many0, many1, separated_list1};
 use nom::sequence::{delimited, pair, preceded, terminated, tuple};
 
-fn parser(s: &str) -> IResult<&str, Vec<Vec<&str>>, (&str, ErrorKind)> {
+// @todo: This would require rewriting the instruction code itself to move the operand-map from the definition to the use
+
+// @todo: Also, allow for
+//@todo: - Labels
+//@todo: - Subroutines
+//@todo: - Comments
+
+fn parse_asm(s: &str) -> IResult<&str, Vec<Vec<&str>>, (&str, ErrorKind)> {
     separated_list1(many0(newline), line)(s)
 }
 
 fn line(s: &str) -> IResult<&str, Vec<&str>, (&str, ErrorKind)> {
-    delimited(multispace0, instruction, space0)(s)
+    delimited(multispace0, instruction_parser, space0)(s)
 }
 
-fn instruction(s: &str) -> IResult<&str, Vec<&str>, (&str, ErrorKind)> {
-    separated_list1(space1, alt((op_keyword, operand)))(s)
+fn instruction_parser(s: &str) -> IResult<&str, Vec<&str>, (&str, ErrorKind)> {
+    separated_list1(space1, alt((operation_keyword, operand_parser)))(s)
 }
 
-fn op_keyword(s: &str) -> IResult<&str, &str, (&str, ErrorKind)> {
-    preceded(space0, take_while1(is_valid_op_keyword))(s)
+fn operation_keyword(s: &str) -> IResult<&str, &str, (&str, ErrorKind)> {
+    preceded(space0, take_while1(is_valid_keyword_character))(s)
 }
 
-fn is_valid_op_keyword(c: char) -> bool {
+fn is_valid_keyword_character(c: char) -> bool {
     is_alphabetic(c as u8) || c == '.' || c == '_'
 }
 
-fn operand(s: &str) -> IResult<&str, &str, (&str, ErrorKind)> {
+fn operand_parser(s: &str) -> IResult<&str, &str, (&str, ErrorKind)> {
     preceded(alt((tag("$"), tag("#"))), digit1)(s)
 }
 
 
 #[cfg(test)]
 mod test {
-    use crate::assembly::parser::parser;
+    use nom::Err::Error;
+    use nom::error::ErrorKind;
+    use nom::IResult;
+
+    use crate::assembly::parser::{instruction_parser, is_valid_keyword_character, operand_parser, operation_keyword, parse_asm};
 
     #[test]
-    pub fn test_sandbox_parsing() {
+    pub fn test_is_valid_keyword_character() {
+        assert!(is_valid_keyword_character('A'));
+        assert!(is_valid_keyword_character('a'));
+        assert!(is_valid_keyword_character('.'));
+        assert!(is_valid_keyword_character('_'));
+
+        assert_eq!(false, is_valid_keyword_character('&'));
+        assert_eq!(false, is_valid_keyword_character('$'));
+        assert_eq!(false, is_valid_keyword_character('`'));
+        assert_eq!(false, is_valid_keyword_character('-'));
+    }
+
+    #[test]
+    pub fn test_operation_keyword_parser() {
+        assert_eq!("LOAD", operation_keyword("LOAD").unwrap().1);
+        assert_eq!("LOAD", operation_keyword(" LOAD").unwrap().1);
+        assert_eq!("LOAD", operation_keyword("LOAD ").unwrap().1);
+        assert_eq!("LOAD", operation_keyword(" LOAD ").unwrap().1);
+        assert_eq!("LOAD", operation_keyword("  LOAD ").unwrap().1);
+        assert_eq!("LOAD", operation_keyword("  LOAD  ").unwrap().1);
+        assert_eq!("LOAD", operation_keyword("\tLOAD        ").unwrap().1);
+    }
+
+    #[test]
+    pub fn test_operand_parser() {
+        assert_eq!(("", "1"), operand_parser("$1").unwrap());
+        assert_eq!(("", "233"), operand_parser("#233").unwrap());
+
+        assert_eq!(
+            operand_parser("^1"),
+            Err(Error(("^1", ErrorKind::Tag)))
+        );
+    }
+
+    #[test]
+    pub fn test_instruction() {
+        assert_eq!(vec!["LOAD", "0", "500"], instruction_parser("LOAD $0 #500").unwrap().1);
+        assert_eq!(vec!["LOAD", "3", "18"], instruction_parser("LOAD #3 $18").unwrap().1);
+    }
+
+    #[test]
+    pub fn test_full_example() {
         let input = r#"
 
                  LOAD    $1   #500
@@ -53,14 +105,11 @@ DIE #1
 
 "#;
 
-        let result = parser(input);
-        match result {
-            Ok((_, good)) => {
-                println!("good");
-            }
-            Err(bad) => {
-                println!("bad")
-            }
-        }
+        let expected = vec![
+            vec!["LOAD", "1", "500"],
+            vec!["ADD", "2", "3", "2"],
+            vec!["DIE", "1"],
+            vec!["HALT"],
+        ];
     }
 }
